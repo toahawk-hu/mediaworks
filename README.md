532e9210d045448193a7001f644777783a55ccc6
Toth Attila <toahawk@gmail.com>
2020-10-20 09:30:20 +0200

    Mediaworks homework

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # mediaworks
+
+
+Töltsd le a node js-t & NPM (https://nodejs.org/en/download/)
+környezeti változó beállítása (NODE_HOME --> c:\Program Files\nodejs\)
+munkakönyvtár létrehozása pl. c:\work\cypress, belépni és itt indítani egy cmd-t
+npm init
+package name --> pl cypresstest (ne cypress maradjon)
+jók a többi default adatok --> elkészül a package.json
+npm install cypress --save-dev
+IDEA telepítése pl. visual studio code
+cypress futtatása (cmd prompt rendszergazdaként) .\node_modules\.bin\cypress open a munkakönyvtárból
+elindul a testrunner. minden tesztesetet tartalmaz, ami az integration\examples-ben van, itt is megtalálható, de megtalálható minden, amit az npm init csinál
+
+
+cypress.json-ba 1400x660-as viewport-ot írtam be, mert azt tapasztaltam, hogy teljesen másképp néz ki az oldal más más felbontáson,
+gondolom okostelefon, tablet stb. miatt, de még a lokátorok se voltak egészen ugyanazok felbontásonként.
+én most 1400x660-ra írtam meg a tesztet, az e-mail-t elküldve saját magamnak azonban nem érkezett új e-mail.
+igyekeztem érthetően kommentelni.
---
+++ cypress/cypress/integration/examples/homeWork.js
@@ -0,0 +1,32 @@
+///reference types= "cypress"/>
+
+describe('Onbox test', () => {
+    it('Testing onboy e-mail client', () => {
+      // Open e-mail client
+      cy.visit("https://www.onbox.hu/")
+
+      // log in to account
+      cy.get(":nth-child(6) > .c02107").click()
+      cy.get ("strong").should("be.visible")
+      cy.get ("#username").type('attila.toth.teszt.mail@onbox.hu')
+      cy.get("#password").type('QO5kaN2pUVQPhKTU')
+      cy.get("#loginBtn").click()
+      
+      // open arrived e-mails
+      cy.get(".c02267 > .c0279 > .c0288", { timeout: 10000 }).should('be.visible');
+      cy.get("[href='#/folder/184906036'] > .c02288").should('be.visible')
+      cy.get("[href='#/folder/184906036'] > .c02288").click()
+      
+      // write e-mail to myself
+      cy.wait(2000)
+      cy.get('.c02259 > .c0249 > .c0268').click()
+      cy.get ('#compose-to').type('attila.toth.teszt.mail@onbox.hu')
+      cy.get ('#subject').type('Automatic test e-mail')
+      cy.get (".c02571 > div").type("This is my a-mail, what I wrote to myself to test whether this mail client sends it to me.")
+      cy.get (".c02607 > .c0249").click()
+      cy.wait(10000)
+      cy.reload()
+      cy.wait(10000)
+      cy.get(":nth-child(2) > .c0225").should("not.be.visible")
+    })
+  })
